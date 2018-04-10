83535f6d20b376de9a8e6b4c68209f127e83be3b
pglez <pglez@users.noreply.github.com>
2018-04-10 12:00:36 +0200

    Add files via upload
    A very limited first version of the virus scanning interface
    The main project is whatthevirus.sh
    tools.sh include some useful functions

---
+++ tools.sh
@@ -0,0 +1,17 @@
+# File that contains functions for antivirus launcher whatthevirus.sh
+# don not execute it directly
+
+simple_menu () {
+  echo "Please, write an option:"
+  echo "1 .- Refresh virus database"
+  echo "2 .- Scan a directory"
+  echo "3 .- Whatever..."
+  read $Option
+exit $Option
+}
+
+provide_dir () {
+  echo "Please, provide a directory to scan"
+  read $Dir
+exit $Dir
+}
---
+++ whatthevirus.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Simple script to lunch antivirus tools
+
+# Include functions from other file
+source tools.sh
+
+# Main body
+Action=$(simple_menu)
+case $Action
+  1 )
+    freshclam
+    ;;
+
+  2 )
+    Directory=$(provide_dir)
+    clamscan $directory
+    ;;
+
+  * )
+    echo "sorry, wong option"
+    exit 1
+    ;;
+
+  esac
+
+  exit 0
