f5ea33bfb77f6f6a50d0d6e2b0616f059673c8fc
javikio <javikio10@gmail.com>
2018-04-11 13:37:54 +0200

    commit 1

--- tools.sh
+++ tools.sh
@@ -1,6 +1,6 @@
 # File that contains functions for antivirus launcher whatthevirus.sh
 # don not execute it directly
-
+#hello
 simple_menu () {
   echo "Please, write an option:"
   echo "1 .- Refresh virus database"
